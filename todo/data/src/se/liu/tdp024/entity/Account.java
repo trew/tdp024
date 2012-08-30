b1ebe3cc774b83a254302a80fd9837f02c940e94
Samuel Andersson <smulster@gmail.com>
2012-08-30 11:06:55 +0200

    Fixed newlines/whitespaces?

--- todo/data/src/se/liu/tdp024/entity/Account.java
+++ todo/data/src/se/liu/tdp024/entity/Account.java
@@ -2,73 +2,74 @@ package se.liu.tdp024.entity;
 
 import java.io.Serializable;
 import javax.persistence.*;
+import se.liu.tdp024.util.EMF;
 
 /**
  *
  */
 @Entity
 public class Account implements Serializable {
-    
+
     public static final int SALARY = 0;
     public static final int SAVINGS = 1;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long accountNumber;
-    
+
     @Column(nullable = false)
     private String personKey;
-    
+
     @Column(nullable = false)
     private String bankKey;
-    
+
     private long amount;
-    
+
     private int accountType;
-    
-    
+
+
     public long getAccountNumber() {
         return accountNumber;
     }
-    
+
     public int getAccountType() {
         return accountType;
     }
-    
+
     public boolean setAccountType(int type) {
-        if (type < 0 || type > 1) 
+        if (type < 0 || type > 1)
             return false;
         accountType = type;
         return true;
     }
-    
+
     public String getPersonKey() {
         return personKey;
     }
-    
+
     public void setPersonKey(String key) {
         personKey = key;
     }
-    
+
     public String getBankKey() {
         return bankKey;
     }
-    
+
     public void setBankKey(String key) {
         bankKey = key;
     }
-    
+
     public long getAmount() {
         return amount;
     }
-    
+
     public void deposit(long amount) {
         this.amount += amount;
     }
-    
+
     public void withdraw(long amount) {
         // TODO: Error checks
         this.amount -= amount;
     }
-    
+
 }

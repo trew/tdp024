20b584d104d4cb97304a3a4a51a4251c7309456f
Björn Ekberg <bjoek022@student.liu.se>
2012-08-31 14:28:11 +0200

    Added class-structure for testing AccountBean

---
+++ todo/logic/test/se/liu/tdp024/logic/test/AccountBeanTest.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.liu.tdp024.logic.test;
+
+import org.junit.*;
+import se.liu.tdp024.entity.Todo;
+import se.liu.tdp024.logic.bean.TodoBean;
+import se.liu.tdp024.logic.util.HTTPHelper;
+import se.liu.tdp024.util.EMF;
+import se.liu.tdp024.logic.bean.AccountBean;
+
+
+public class AccountBeanTest {
+
+    public AccountBeanTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void testCreate() {
+        
+    }
+
+    @Test
+    public void testDeposit() {
+
+    }
+
+    @Test
+    public void testWithdraw() {
+
+    }
+
+    @Test
+    public void testGetAccount() {
+
+    }
+
+    @Test
+    public void testFindByBankKey() {
+
+    }
+
+    @Test
+    public void testFindByPersonKey() {
+
+    }
+
+
+
+}

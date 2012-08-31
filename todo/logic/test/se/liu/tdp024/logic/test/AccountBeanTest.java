cf4ecd615b12a3fb5324304e9f50e07db8ee6206
Björn Ekberg <bjoek022@student.liu.se>
2012-08-31 15:49:32 +0200

    Added test for Account.Create, incomplete.

--- todo/logic/test/se/liu/tdp024/logic/test/AccountBeanTest.java
+++ todo/logic/test/se/liu/tdp024/logic/test/AccountBeanTest.java
@@ -5,10 +5,9 @@
 package se.liu.tdp024.logic.test;
 
 import org.junit.*;
-import se.liu.tdp024.entity.Todo;
-import se.liu.tdp024.logic.bean.TodoBean;
+import com.google.gson.*;
+import se.liu.tdp024.entity.Account;
 import se.liu.tdp024.logic.util.HTTPHelper;
-import se.liu.tdp024.util.EMF;
 import se.liu.tdp024.logic.bean.AccountBean;
 
 
@@ -36,7 +35,26 @@ public class AccountBeanTest {
 
     @Test
     public void testCreate() {
-        
+
+
+        String responseP = HTTPHelper.get("http://enterprise-systems.appspot.com/person/find.name", "name", "Zorro");
+        Assert.assertFalse("Testperson not available, cannot do test", responseP==null);
+
+        JsonParser jp = new JsonParser();
+        JsonElement jeP = jp.parse(responseP);
+        JsonObject joP = jeP.getAsJsonObject();
+        String personKey = joP.get("key").getAsString();
+
+        String responseB = HTTPHelper.get("http://enterprise-systems.appspot.com/bank/find.name", "name", "SWEDBANK");
+        Assert.assertFalse("Testbank not available, cannot do test", responseB==null);
+
+        JsonElement jeB = jp.parse(responseB);
+        JsonObject joB = jeB.getAsJsonObject();
+        String bankKey = joB.get("key").getAsString();
+
+        Assert.assertTrue(AccountBean.create(Account.SALARY, personKey, bankKey) != 0);
+
+
     }
 
     @Test

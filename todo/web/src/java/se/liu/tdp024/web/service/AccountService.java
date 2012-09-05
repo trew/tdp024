49ead3bc2733190c0d7f1471e15d69326062ee0d
Samuel Andersson <smulster@gmail.com>
2012-09-05 12:49:59 +0200

    Adds initial service functions. All return 500
     - Create, listbypersonid and listbybankid

--- todo/web/src/java/se/liu/tdp024/web/service/AccountService.java
+++ todo/web/src/java/se/liu/tdp024/web/service/AccountService.java
@@ -7,11 +7,39 @@ import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import se.liu.tdp024.entity.Account;
+import se.liu.tdp024.entity.Todo;
 import se.liu.tdp024.logic.bean.AccountBean;
+import se.liu.tdp024.logic.bean.TodoBean;
 
 @Path("/account")
 public class AccountService {
 
     private static final Gson GSON = new Gson();
 
+    @GET
+    @Path("/create")
+    public Response createAccount(
+            @QueryParam("personid") String personID,
+            @QueryParam("bankid") String bankID,
+            @QueryParam("type") String accountType) {
+
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+
+    }
+
+    @GET
+    @Path("/list.personid")
+    public Response listByPersonID(@QueryParam("id") String id) {
+
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+
+    }
+
+    @GET
+    @Path("/list.bankid")
+    public Response listByBankID(@QueryParam("id") String id) {
+
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+
+    }
 }
